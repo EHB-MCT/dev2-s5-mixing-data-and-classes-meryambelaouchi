a6a8ef66633ad60cd8f4dbacaabdbcec5c49378a
Meryam Belaouchi <meryam.belaouchi@student.ehb.be>
2023-03-22 16:39:18 +0100

    something works

--- js/index.js
+++ js/index.js
@@ -2,13 +2,16 @@
 
 const app = {
     items: [],
-    init() {},
+    init() {
+        this.getData();
+    },
     getData() {
 
 
         fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&amount=10')
-            .then(function (jokes) {
-                return jokes.json();
+            .then(function (response) {
+                console.log(response);
+                return response.json();
             })
             .then(function (data) {
                 console.log(data);
